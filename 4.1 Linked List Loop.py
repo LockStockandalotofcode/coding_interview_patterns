24bf0d8c67c8eed63fada6f2fcbf7f01605ede3b
LockStockandalotofcode <shivanie0101@gmail.com>
2026-06-24 23:51:25 +0530

    4.1

---
+++ 4.1 Linked List Loop.py
@@ -0,0 +1,24 @@
+from ds import ListNode
+
+"""
+Definition of ListNode:
+class ListNode:
+    def __init__(self, val=None, next=None):
+        self.val = val
+        self.next = next
+"""
+
+def linked_list_loop(head: ListNode) -> bool:
+    if not head or not head.next:
+        return False
+    
+    slow = head
+    fast = head
+
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            return True
+    
+    return False
