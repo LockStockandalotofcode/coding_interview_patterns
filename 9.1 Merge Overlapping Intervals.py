22744194d6fad4fbd3575eaede552cc6d8312747
LockStockandalotofcode <shivanie0101@gmail.com>
2026-06-28 22:26:46 +0530

    TIME COMPLEXITY O(nlog(n)), SPACE COMPLEXITY O(n), n number of intervals

---
+++ 9.1 Merge Overlapping Intervals.py
@@ -0,0 +1,23 @@
+from ds import Interval
+from typing import List
+
+"""
+Definition of Interval:
+class Interval:
+    def __init__(self, start: int, end: int):
+        self.start = start
+        self.end = end
+"""
+
+def merge_overlapping_intervals(intervals: List[Interval]) -> List[Interval]:
+    intervals.sort(key=lambda x: x.start)
+    merged = [intervals[0]]
+    for B in intervals[1: ]:
+        A = merged[-1]
+
+        # overlap
+        if B.start <= A.end:
+            merged[-1] = Interval(A.start, max(A.end, B.end))
+        else:
+            merged.append(B)
+    return merged
